921816991479b04241a2d42db4ad3de56e5a1eb3
adai183 <andreas.daiminger@gmail.com>
2017-02-20 09:18:42 +0100

    cmmit

--- analyze_data.py
+++ analyze_data.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 csv_path = 'Data/train_data.csv'
 df = pd.DataFrame.from_csv(csv_path)
-df.hist(column='steering',  bins=1000)
-plt.show()
+# df.hist(column='steering',  bins=1000)
+# plt.show()
 counts = df['steering'].value_counts()
+print (counts)
+print(df['steering'].value_counts().idxmax())
+print(df['steering'].value_counts().max())
+print (counts.loc[0.0])
